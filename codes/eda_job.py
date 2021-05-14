a3a7ce14ddeaa9208f1b1133c4f78502a3db7fa4
matheus695p <mateu_695p@hotmail.com>
2021-05-14 16:41:18 -0400

    add: analisis exploratorio

--- codes/eda_job.py
+++ codes/eda_job.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from src.eda.exploratory_data_analysis import profiling_report
-from src.utils.visualizations import pichart_plot
+from src.utils.visualizations import (piechart_plot, pairplot_df,
+                                      correlation_matrix)
 # hacer una exploración rápida con pandas profiling, dado la naturaleza de
 # la prueba
 path = "data/creditcard_cleaned.csv"
@@ -12,6 +13,19 @@ df = pd.read_csv(path)
 
 # se puede ver mucho desbalance de clases en los datos, es mejor tratarlo
 # como un problema de detección de anomalias
-pichart_plot(df, col="class")
+piechart_plot(df, col="class")
 
+# ver correlaciones
+correlation_matrix(df, method="pearson")
+correlation_matrix(df, method="spearman")
+correlation_matrix(df, method="kendall")
+
+# [NO ES NECESARIO DE ACUERDO A LAS MATRICES DE CORRELACIONES]
 # pairplot de las variables
+for col1 in df.columns:
+    for col2 in df.columns:
+        if col1 == col2:
+            pass
+        else:
+            print("Scatter plot para ver correlaciones")
+            pairplot_df(df, col1, col2, path="results/scatter-plot")
